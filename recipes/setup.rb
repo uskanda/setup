edff822980aa1136c911288e7dc59180c8b1cb70
Yusuke Kanda <uskanda@gmail.com>
2015-08-19 01:19:43 +0900

    add homesick

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,2 +1,18 @@
 include_recipe 'homebrew::package'
 include_recipe 'homebrew::cask'
+
+#homesick
+execute "Install homesick" do
+  command "gem install homesick"
+  not_if "test $(which homesick)"
+end
+
+node["homesick"]["castles"].each do |castle|
+  castle_basename = File.basename(castle)
+  execute "Add Castle" do
+    command "homesick clone #{castle};\
+             homesick symlink #{castle_basename}"
+    not_if "homesick list | grep -q '#{castle_basename}'"
+  end
+end
+
