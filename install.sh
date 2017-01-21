2674485a986aa2942c4da776c4303ee55ba2637b
Yusuke Kanda <uskanda@gmail.com>
2017-01-22 05:07:17 +0900

    add debug mode (arg "-d")

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+while getopts d OPT
+do
+  case $OPT in
+    "d" ) MITAMAE_OPTS="-l debug" ;;
+  esac
+done
 
 cd $(dirname $0)
 
@@ -8,7 +14,7 @@ bin/install-mitamae
 if [ "$(uname)" == 'Darwin' ]; then
   bin/mitamae local recipes/setup_macos.rb -y nodes/local.yml -l debug
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  bin/mitamae local recipes/setup_linux.rb -y nodes/local_linux.yml
+  bin/mitamae local recipes/setup_linux.rb -y nodes/local_linux.yml $MITAMAE_OPTS
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
