87397f2c220f9adfeaaa68e4a2dbdc5c597abdaf
Yusuke Kanda <uskanda@gmail.com>
2017-01-22 07:39:53 +0900

    separete homeshick

---
+++ recipes/homeshick.rb
@@ -0,0 +1,18 @@
+home_dir = ENV["HOME"]
+homeshick_dir = "#{home_dir}/.homesick/repos/homeshick"
+git homeshick_dir do
+  repository "git://github.com/andsens/homeshick.git"
+end
+
+node["homesick"]["castles"].each do |castle|
+  castle_basename = File.basename(castle)
+  execute "Add Castle" do
+    command "#{homeshick_dir}/bin/homeshick clone #{castle};\
+             #{homeshick_dir}/bin/homeshick symlink #{castle_basename}"
+    not_if "#{homeshick_dir}/bin/homeshick list | grep -q '#{castle_basename}'"
+  end
+  execute "Update Castles" do
+    command "#{homeshick_dir}/bin/homeshick pull"
+    only_if "#{homeshick_dir}/bin/homeshick check | grep -q behind"
+  end
+end
