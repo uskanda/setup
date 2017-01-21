18365a851858d8770ca4b9de590108524d1d23fb
Yusuke Kanda <uskanda@gmail.com>
2017-01-22 04:00:00 +0900

    add homeshick in ubuntu

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ bin/install-mitamae
 if [ "$(uname)" == 'Darwin' ]; then
   bin/mitamae local recipes/setup_macos.rb -y nodes/local.yml -l debug
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  bin/mitamae local recipes/setup_linux.rb
+  bin/mitamae local recipes/setup_linux.rb -y nodes/local_linux.yml
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
--- recipes/setup_linux.rb
+++ recipes/setup_linux.rb
@@ -4,6 +4,22 @@ execute "apt-get-update" do
   only_if "exit $(( `stat -c %Y /var/lib/apt/lists` > (`date +%s` - 86400) ))"
 end
 
+home_dir = ENV["HOME"]
+homeshick_dir = "#{home_dir}/.homesick/repos/homeshick"
+git homeshick_dir do
+  repository "git://github.com/andsens/homeshick.git"
+end
+
+node["homesick"]["castles"].each do |castle|
+  castle_basename = File.basename(castle)
+  execute "Add Castle" do
+    command "#{homeshick_dir}/bin/homeshick clone #{castle};\
+             #{homeshick_dir}/bin/homeshick symlink #{castle_basename}"
+    not_if "#{homeshick_dir}/bin/homeshick list | grep -q '#{castle_basename}'"
+  end
+end
+
+
 packages = %w[vim]
 packages.each do |package_name|
   package package_name do
